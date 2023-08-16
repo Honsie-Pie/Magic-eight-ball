da76fa31b9efb0630914dba92ad652fbb91643a7
Miguel Jaramillo <m.jrillo@live.com>
2023-08-16 16:04:19 -0600

    add unfolding effect

--- src/answer.js
+++ src/answer.js
@@ -3,7 +3,7 @@ import Ball from './bal.js';
 export default function AnswerSpace(props){
     return(
         <div className="answer-space">
-            <h1 className="output-question">{props.question}</h1>
+            <h1 className={props.blur? "output-question blurred-question": "output-question"}>{props.question}</h1>
             <Ball answer={props.answer} anim={props.anim} blur={props.blur}/>
             <div className="ball-shadow"></div>
         </div>
